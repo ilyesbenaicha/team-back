702fd228d5b46593b335c01a0a446cec0821beb6
ilyes ben aicha <ilyesbenaicha09@gmail.com>
2022-05-25 00:36:46 +0100

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 const {errorHandler} = require('./src/middleware/errorMiddleware')
 const connectDB = require('./src/config/db')
 const port = process.env.PORT || 5000
@@ -8,7 +9,10 @@ connectDB()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
+app.use(cors(
+{    origin: "http://localhost:3000",
+    credentials: true
+}));
 app.use('/api/user', require('./src/routes/userRoutes'))
 app.use ('/api/project', require('./src/routes/projectRoutes'))
 app.use(errorHandler)
