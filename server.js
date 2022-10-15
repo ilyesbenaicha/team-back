9f8fc831a5b5825fe0855a0b1280c4b1bd0dbd75
ilyes ben aicha <ilyesbenaicha09@gmail.com>
2022-10-15 21:27:55 +0100

    confuser

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:3000', 'http://www.teams.mobelite.fr/' ];
+    const allowedOrigins = ["http://localhost:3000", "http://www.teams.mobelite.fr/" ];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
          res.setHeader('Access-Control-Allow-Origin', origin);
